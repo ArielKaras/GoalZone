6e0b3fcf53014fc5e79f7eda5e95bfc784ac3fa5
ariel <ariel1434@gmail.com>
2023-02-16 11:07:00 +0200

    Creating flask package structure

---
+++ landing_page/models.py
@@ -0,0 +1,3 @@
+"""Use a separate file for database models:
+ Store your database models in a separate file like models.py.
+This file should contain the SQLAlchemy models that represent your database tables."""
---
+++ landing_page/views/main.py
@@ -0,0 +1,3 @@
+"""
+Use views to define routes: In Flask, a view is a Python function that is mapped to a URL.
+"""
