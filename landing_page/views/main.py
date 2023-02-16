c24f455511ea2ee03b6a5aac9192da751a2d4664
ariel <ariel1434@gmail.com>
2023-02-16 16:14:04 +0200

    update index.html in Flask app
    Update landing page and fix Excel file issue
    
    - Updated landing page HTML to display in Hebrew
    - Fixed issue with Excel file creation that was causing a new file to be created each time the app was executed
    - Users can now register with their name and phone number, and the data is correctly added to the existing Excel file
    
    Further testing is still needed to ensure that the app functions correctly under all conditions.

--- landing_page/views/main.py
+++ landing_page/views/main.py
@@ -5,6 +5,7 @@ In Flask, a view is a Python function that is mapped to a URL.
 
 from flask import Flask, request, render_template
 import pandas as pd
+import os
 
 app = Flask(__name__)
 
@@ -19,9 +20,19 @@ def register():
     name = request.form['name']
     phone = request.form['phone']
 
-    # Save the user's information to an Excel file
+    # Create a DataFrame with the new data
     data = {'Name': [name], 'Phone': [phone]}
     df = pd.DataFrame(data)
-    df.to_excel('Students.xlsx', index=True)
+
+    # Check if the Excel file already exists
+    if os.path.isfile('Students.xlsx'):
+        # If it exists, load the existing data into a DataFrame
+        existing_df = pd.read_excel('Students.xlsx')
+
+        # Append the new data to the existing DataFrame
+        df = pd.concat([existing_df, df])
+
+    # Save the updated DataFrame to the Excel file
+    df.to_excel('Students.xlsx', index=False)
 
     return 'Thank you for registering!'
